a756198ba1418c69e758e1732b61d22cf5013fcd
zuramai <saugi.dev@gmail.com>
2022-08-17 14:14:25 +0700

    feat: `use_counter` rust

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,10 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
-import init, { render_app } from "hello"
+import init, { use_counter } from "hello"
 
-const initWasm = await init()
+await init()
 
-let app = render_app("#app")
-console.log(app)
-// `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
+const counterBtn = use_counter()
+const card = document.querySelector('.card')
+
+card?.appendChild(counterBtn)
+
